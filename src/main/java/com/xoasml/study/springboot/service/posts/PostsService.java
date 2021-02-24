fecc9d274c6084db9ec9fe7fbc5285c8058748be
TaeHoonKim <xoasml@gmail.com>
2021-02-25 02:10:05 +0900

    index화면에 글 목록 추가

--- src/main/java/com/xoasml/study/springboot/service/posts/PostsService.java
+++ src/main/java/com/xoasml/study/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.xoasml.study.springboot.service.posts;
 
 import com.xoasml.study.springboot.domain.posts.Posts;
 import com.xoasml.study.springboot.domain.posts.PostsRepository;
+import com.xoasml.study.springboot.web.dto.PostsListResponseDto;
 import com.xoasml.study.springboot.web.dto.PostsResponseDto;
 import com.xoasml.study.springboot.web.dto.PostsSaveRequestDto;
 import com.xoasml.study.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -39,4 +42,13 @@ public class PostsService {
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+
 }
--- src/main/java/com/xoasml/study/springboot/web/IndexController.java
+++ src/main/java/com/xoasml/study/springboot/web/IndexController.java
@@ -1,16 +1,20 @@
 package com.xoasml.study.springboot.web;
 
+import com.xoasml.study.springboot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
-    
-    @GetMapping("/")
-    public String index() {
 
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
 
     }
@@ -19,5 +23,6 @@ public class IndexController {
     public String postsSave() {
         return "posts-save";
     }
+
 
 }
